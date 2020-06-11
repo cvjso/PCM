e554810287132765fdd8a27835ebc0927fc9731c
tenonin <luante2000@gmail.com>
2020-06-11 12:37:27 -0300

    Need some replaces in updateMachine (Crud finalizado, falta so o update)
    alguem ajuda aki

--- PCM_back/src/main/java/com/example/pcm/model/Response.java
+++ PCM_back/src/main/java/com/example/pcm/model/Response.java
@@ -6,8 +6,10 @@ import java.util.Optional;
 
 public class Response {
     private ArrayList<User> users = new ArrayList<User>();
+    private ArrayList<Machine> machines = new ArrayList<Machine>();
     private String msg;
     private Optional<User> optional_user;
+    private Optional<Machine> optional_machine;
 
     public ArrayList<User> getUsers() {
         return users;
@@ -17,8 +19,20 @@ public class Response {
         return optional_user;
     }
 
+    public ArrayList<Machine> getMachines(){
+        return machines;
+    }
+
+    public Optional<Machine> getOptional_machine(){
+        return optional_machine;
+    }
+
     public void setOptional_user(Optional<User> optional_user) {
         this.optional_user = optional_user;
+    }
+
+    public void setOptional_machine(Optional<Machine> optional_machine){
+        this.optional_machine = optional_machine;
     }
 
     public String getMsg() {
@@ -31,6 +45,10 @@ public class Response {
 
     public void setUsers(List<User> list) {
         this.users = (ArrayList<User>) list;
+    }
+
+    public void setMachines(List<Machine> list){
+        this.machines = (ArrayList<Machine>) list;
     }
     
 }
